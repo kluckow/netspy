3416634fd05055315636fdb1176812a78a9d9e3d
Markus Kluckow <kluckow@cominto.de>
2016-01-27 16:14:43 +0100

    added test-src folder for experiments while developing

---
+++ test-src/netspy/NetSpyTest.java
@@ -0,0 +1,23 @@
+package netspy;
+
+/**
+ * Class NetSpyTest.
+ *
+ * <p>
+ * Class for testing some functionality of the NetSpy
+ * </p>
+ *
+ * @author kluckow
+ */
+public class NetSpyTest {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+
+        // Show the main window
+        final MyJFrame mainFrame = new MyJFrame();
+    }
+
+}
