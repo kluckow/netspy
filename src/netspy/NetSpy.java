a7189d271076d6db1345eb192ad27183643a80b3
Markus Kluckow <markus.kluckow@googlemail.com>
2016-02-01 08:13:19 +0100

    removed unused methods

--- src/netspy/NetSpy.java
+++ src/netspy/NetSpy.java
@@ -22,40 +22,9 @@ public class NetSpy {
      */
     public static void main(final String[] args) {
 
-        // TODO: find way to extract email date format and convert to customized format
-        // 1. create static final DateFormat for log lines
-        // DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
-
-        // 2. create a Date from the extracted string
-        // Date sendingDate = new Date("mon, 22 aug 2005 20:21:52 +0200");
-        // System.out.println("Format des Datums in Emails: " + sendingDate);
-
-        // 3. format the extracted Date with your log line DateFormat
-        // String date = dateFormat.format(sendingDate);
-        // 4. the Result is a
-        // System.out.println("Format des Datums umgeformt: " + date);
-
         if (checkInboxForMails()) {
             processMailsInInbox();
-            // // TODO: implement logging
-            // logScanResults();
         }
-    }
-
-    /**
-     * Log scan results.
-     */
-    @SuppressWarnings("unused")
-    private static void logScanResults() {
-
-    }
-
-    /**
-     * Report.
-     */
-    @SuppressWarnings("unused")
-    private static void report() {
-
     }
 
     /**
@@ -76,17 +45,6 @@ public class NetSpy {
     }
 
     /**
-     * Display menu.
-     */
-    @SuppressWarnings("unused")
-    private static void displayMenu() {
-
-        /**
-         * display options for: 1) Scan Mails 2) Blacklist editor 3) Whitelist editor
-         */
-    }
-
-    /**
      * Check inbox for mails.
      *
      * @return true, if successful
@@ -94,6 +52,7 @@ public class NetSpy {
     private static boolean checkInboxForMails() {
 
         if (!emailHandler.checkMailbox()) {
+        	// TODO: implement ErrorNotificationPopup
             System.out.println("Keine Emails in der Mailbox gefunden. Keine �berpr�fung notwendig.");
             return false;
         } else {
@@ -105,12 +64,15 @@ public class NetSpy {
                     // extract content of emailFile and create an email object with that content
                     final Email email = new Email(emailHandler.getMailContent(mailFile),
                         mailFile.getPath());
-
                     // add email object to email container
                     emailHandler.getMailContainer().getMails().add(email);
                 } catch (final IOException e) {
-                    System.out.println(e.getMessage());
+                	// TODO: implement ErrorNotificationPopup
                 }
+            }
+            // set indices of emails
+            for (Email email : emailHandler.getMailContainer().getMails()) {
+            	email.setIndex(emailHandler.getMailContainer().getMails().indexOf(email));
             }
             return true;
         }
