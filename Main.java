a206e2b355b974aab9fe6b6943624262d69617cf
kluckow <markus.kluckow@googlemail.com>
2016-02-21 21:42:21 +0100

    drag and drop test

---
+++ Main.java
@@ -0,0 +1,24 @@
+/**
+ * (c) Copyrights 2016 by Kevin Schorn, Markus Kluckow
+ */
+package netspy;
+
+import netspy.components.gui.components.frame.NetSpyFrame;
+
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		
+		new NetSpyFrame();
+	}
+	
+
+}
